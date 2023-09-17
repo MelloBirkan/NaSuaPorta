2e2fddcf6d145996d340cc4b2ad4f85e0817f73a
Mello Birkan <mgbirkan@icloud.com>
2023-09-16 23:01:17 -0300

    Initial Commit

---
+++ NaSuaPorta/NaSuaPortaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaSuaPortaApp.swift
+//  NaSuaPorta
+//
+//  Created by Marcello Gonzatto Birkan on 16/09/23.
+//
+
+import SwiftUI
+
+@main
+struct NaSuaPortaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
