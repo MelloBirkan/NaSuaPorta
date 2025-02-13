89b4810bb7371a15f51a945d46f35723e467c344
Marcello Gonzatto Birkan <mgbirkan@icloud.com>
2025-02-13 16:28:32 -0300

    Add store logos and StoreItemView component

---
+++ NaSuaPorta/StoresView/StoreItemView.swift
@@ -0,0 +1,29 @@
+//
+//  StoreItemView.swift
+//  NaSuaPorta
+//
+//  Created by Marcello Gonzatto Birkan on 13/02/25.
+//
+
+import SwiftUI
+
+struct StoreItemView: View {
+  let order: OrderType
+  
+  var body: some View {
+    Label {
+      Text(order.name)
+        .font(.subheadline)
+    } icon: {
+      Image(order.image)
+        .resizable()
+        .scaledToFit()
+        .clipShape(Circle())
+        .frame(width: 50, height: 50)
+    }
+  }
+}
+
+#Preview {
+  StoreItemView(order: OrderType(id: 3, name: "Coffee Co", image: "CoffeeCo"))
+}
